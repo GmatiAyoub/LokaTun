aff34f67b6742147dccaa38976abfa3b0158646b
Gmati Ayoub <gmtayoyoub15@gmail.com>
2026-07-02 13:07:44 +0200

    sprint 4 , evaluation et notation terminer avec succes avec test avec postmen et le front

--- src/index.js
+++ src/index.js
@@ -5,28 +5,26 @@ const cors = require('cors');
 const authRoutes = require('./routes/auth.routes');
 const annonceRoutes = require('./routes/annonce.routes');
 const reservationRoutes = require('./routes/reservation.routes');
+const evaluationRoutes = require('./routes/evaluation.routes');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ─── Middlewares globaux ───────────────────
 app.use(cors());
 app.use(express.json());
 
-// ─── Routes ───────────────────────────────
 app.use('/api/auth', authRoutes);
 app.use('/api/annonces', annonceRoutes);
 app.use('/api/reservations', reservationRoutes);
-// Route de santé (vérifier que le serveur tourne)
+app.use('/api/evaluations', evaluationRoutes);
+
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', message: 'Lokatun API is running 🚀' });
 });
 
-// ─── Gestion des erreurs (toujours en dernier) ──
 app.use(errorHandler);
 
-// ─── Démarrage ────────────────────────────
 app.listen(PORT, () => {
   console.log(`✅ Serveur Lokatun démarré sur le port ${PORT}`);
 });
